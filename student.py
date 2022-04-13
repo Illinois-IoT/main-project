a3160821e79e68b4b6968e10bcf7d1a184b2e324
Shlok Mehrotra <shlokmehrotra@gmail.com>
2022-04-13 17:21:40 -0500

    edit student file

---
+++ student.py
@@ -0,0 +1,14 @@
+import project as proj
+
+#proj.play_sound(file_name = "patrick.WAV", length = "5", volume = "50")
+
+#proj.clear_images()
+
+#proj.capture_images("yay")
+
+proj.convert_to_grayscale("yay.jpg")
+
+
+
+
+
