42cbfcb93da72144127377cc26bdffe033cc5293
mingjie7 <mingjie7@illinois.edu>
2022-04-27 22:11:01 -0500

    Fixed format & updated push_button_pic

--- push_botton_pic.py
+++ push_botton_pic.py
@@ -1,9 +1,13 @@
-import sys,os,time
+import sys
+import os
+import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10,GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-while True:	
-    if(GPIO.input(10)==GPIO.HIGH):
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    if(GPIO.input(10) == GPIO.LOW):
         print("Hello")
+        time.sleep(0.5)
