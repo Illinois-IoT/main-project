705779d87a3a65fbf39ee47254b9740227421e96
mingjie7 <mingjie7@illinois.edu>
2022-06-05 17:41:04 -0500

    feat: test live transcription

--- camera-stream-flask/main.py
+++ camera-stream-flask/main.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, Response, request
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
+import subprocess
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -17,7 +18,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def get_feed(camera):
+def get_camera_frame(camera):
     #get camera frames
     for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
         image = frame.array
@@ -27,17 +28,24 @@ def get_feed(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + image.tobytes() + b'\r\n\r\n')
 
+def get_live_transcription():
+    popen = subprocess.Popen("spchcat", stdout=subprocess.PIPE)
+    for stdout_line in iter(popen.stdout.readline, b""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(get_feed(camera),
+    return Response(get_camera_frame(camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/upload', methods = ['POST'])
-def upload_audio():
-    if request.method == 'POST':
-        f = request.files['audio_data']
-        f.save(f.filename)
-        return "File saved successfully"
+@app.route('/transcript_feed')
+def transcript_feed():
+    return Response(get_live_transcription(camera),
+                    mimetype='mimetype='text/html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, ssl_context="adhoc")
+    app.run(host='0.0.0.0', debug=False)
